a98ec86bf8b3cd693d2ac9cddff63e4b4f039d2a
pomniang04 <moustapha.niang@pomerleau.ca>
2025-04-08 15:42:49 -0400

    v1 + test

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,4 +1,4 @@
-import {test} from 'vitest'
+import { test } from 'vitest';
 import "@k2oss/k2-broker-core/test-framework";
 import "./index";
 
@@ -20,52 +20,59 @@ test('describe returns the hardcoded instance', async (t) => {
 
     t.expect(schema).toStrictEqual({
         objects: {
-            todo: {
-                displayName: "TODO",
-                description: "Manages a TODO list",
+            document: {
+                displayName: "Document",
+                description: "Manages document generation",
                 properties: {
                     id: {
                         displayName: "ID",
                         type: "number",
                     },
-                    userId: {
-                        displayName: "User ID",
-                        type: "number",
-                    },
-                    title: {
-                        displayName: "Title",
+                    name: {
+                        displayName: "Name",
                         type: "string",
                     },
-                    completed: {
-                        displayName: "Completed",
-                        type: "boolean",
+                    type: {
+                        displayName: "Type",
+                        type: "string",
+                    },
+                    base64Data: {
+                        displayName: "Base64 Data",
+                        type: "string",
+                    },
+                    jsonData1: {
+                        displayName: "JSON Data 1",
+                        type: "string",
+                    },
+                    jsonData2: {
+                        displayName: "JSON Data 2",
+                        type: "string",
+                    },
+                    jsonData3: {
+                        displayName: "JSON Data 3",
+                        type: "string",
+                    },
+                    jsonData4: {
+                        displayName: "JSON Data 4",
+                        type: "string",
+                    },
+                    documentType: {
+                        displayName: "Document Type",
+                        type: "string",
                     },
                 },
                 methods: {
-                    get: {
-                        displayName: "Get TODO",
-                        type: "read",
-                        inputs: ["id"],
-                        outputs: ["id", "userId", "title", "completed"],
-                    },
-                    getParams: {
-                        displayName: "Get TODO",
-                        type: "read",
-                        parameters: {
-                            pid: {
-                                displayName: "param1",
-                                description: "Description Of Param 1",
-                                type: "number",
-                            },
-                        },
-                        requiredParameters: ["pid"],
-                        outputs: ["id"],
+                    generate: {
+                        displayName: "Generate Document",
+                        type: "execute",
+                        inputs: ["id", "name", "type", "base64Data", "jsonData1", "jsonData2", "jsonData3", "jsonData4", "documentType"],
+                        outputs: ["jsonResult"],
                     },
                 },
             },
         },
     });
-})
+});
 
 test("execute fails with the wrong parameters", async (t) => {
     await t.expect(
@@ -74,14 +81,14 @@ test("execute fails with the wrong parameters", async (t) => {
             methodName: "unused",
             parameters: {},
             properties: {},
-            configuration: {},
-            schema: {},
+            configuration: {} as any,
+            schema: {} as any,
         })
     ).rejects.toThrow("The object test1 is not supported.");
 
     await t.expect(
         onexecute({
-            objectName: "todo",
+            objectName: "document",
             methodName: "test2",
             parameters: {},
             properties: {},
@@ -101,94 +108,103 @@ test("execute passes with method params", async (t) => {
 
     await Promise.resolve<void>(
         onexecute({
-            objectName: "todo",
-            methodName: "getParams",
-            parameters: {
-                pid: 456,
-            },
-            properties: {},
-            configuration: {},
-            schema: {},
-        })
-    );
-
-    t.expect(result).toStrictEqual({id:456})
-});
-
-test("execute passes", async (t) => {
-    let xhr: { [key: string]: any } = null;
-    class XHR {
-        public onreadystatechange: () => void;
-        public readyState: number;
-        public status: number;
-        public responseText: string;
-        private recorder: { [key: string]: any };
-
-        constructor() {
-            xhr = this.recorder = {};
-            this.recorder.headers = {};
-        }
-
-        open(method: string, url: string) {
-            this.recorder.opened = { method, url };
-        }
-
-        setRequestHeader(key: string, value: string) {
-            this.recorder.headers[key] = value;
-        }
-
-        send() {
-            queueMicrotask(() => {
-                this.readyState = 4;
-                this.status = 200;
-                this.responseText = JSON.stringify({
-                    id: 123,
-                    userId: 51,
-                    title: "Groceries",
-                    completed: false,
-                });
-                this.onreadystatechange();
-                delete this.responseText;
-            });
-        }
-    }
-
-    mock("XMLHttpRequest", XHR);
-
-    let result: any = null;
-    function pr(r: any) {
-        result = r;
-    }
-
-    mock("postResult", pr);
-
-    await Promise.resolve<void>(
-        onexecute({
-            objectName: "todo",
-            methodName: "get",
+            objectName: "document",
+            methodName: "generate",
             parameters: {},
             properties: {
                 id: 123,
+                name: "Test Document",
+                type: "mbh",
+                base64Data: "base64string",
+                jsonData1: JSON.stringify({ 
+                    UCI: "value1", 
+                    NCRStatus: "status", 
+                    InitiatorName: "name", 
+                    InitiatorEmail: "email", 
+                    NCRRequestDate: "2023-04-01", 
+                    NCRContractNumber: "contractNumber", 
+                    NCRLot: "lot",
+                    NCRProjectNumber: "projectNumber",
+                    ProjectTitle: "title",
+                    NCRContractTitle: "contractTitle",
+                    STMDesignRepresentative: "designRep", 
+                    RepresentativeAdjudicateur: "repAdjudicateur", 
+                    NCRIssuer: "issuer", 
+                    NCRSector: "sector", 
+                    NCRWBS: "wbs", 
+                    NCRDiscipline: "discipline", 
+                    MBH_NCR_For: "for", 
+                    MBH_NCR_Activity: "activity", 
+                    MBH_NCR_IsSubcontractor: "isSubcontractor", 
+                    MBH_NCR_SousActivity: "sousActivity", 
+                    WBSTEXT: "wbsText", 
+                    MBH_NCR_Reference: "reference", 
+                    NCRCategory: "category", 
+                    NCRTitle: "title", 
+                    NCRDate: "2023-04-01", 
+                    NCROtherReference: "otherReference", 
+                    NCRResponseRequiredBy: "2023-04-01", 
+                    MBH_NCR_Type: "type", 
+                    MBH_NCR_SubType: "subType", 
+                    NCRDescription: "description", 
+                    MBH_NCR_ProjectManagerEmail: "projectManagerEmail", 
+                    MBH_NCR_ImpactPlanAsBuilt: "impact" 
+                }),
+                jsonData2: JSON.stringify({ value: [{ MBH_NCR_Description: "description", MBH_NCR_ActionPICEmail: "actionPICEmail", MBH_NCR_PlanDate: "2023-04-01", MBH_NCR_Response: "response", MBH_NCR_ExectionDate: "2023-04-01", MBH_NCR_NCRID_fk: "23", MBH_NCR_ActionPlanApprovalStatusByQuality: "approvalStatusByQuality", MBH_NCR_ActionPlanApprovalStatusByClient: "approvalStatusByClient", MBH_NCR_ActionResponseApprovalStatusByPlanPIC: "Oui", MBH_NCR_ActionResponseApprovalStatusByClient: "approvalStatusByClient" }] }),
+                jsonData3: JSON.stringify({ value: [{ ODataProperty__GCPC_NCR_PICEmail: "picEmail", GCPC_NCR_ResponseDate: "2023-04-01", GCPC_NCR_Step: "step", GCPC_NCR_Comment: "comment" }] }),
+                jsonData4: JSON.stringify({ value: [{ ID: "1", NCR_ID: "23", FileName: "fileName", FileDescription: "fileDescription", Comment: "comment", UploadedBy: "uploadedBy", Upload_Date: "2023-04-01", Step: "step", "File@odata.mediaReadLink": "mediaReadLink" }] }),
+                documentType: "pdf",
             },
             configuration: {},
             schema: {},
         })
     );
 
-    t.expect(xhr).toStrictEqual({
-        opened: {
-            method: "GET",
-            url: "https://jsonplaceholder.typicode.com/todos/123",
-        },
-        headers: {
-            test: "test value",
-        },
-    });
-
     t.expect(result).toStrictEqual({
-        id: 123,
-        userId: 51,
-        title: "Groceries",
-        completed: false,
+        jsonResult: JSON.stringify({
+            instance_A: "value1",
+            instance_B: "MBH NCR",
+            instance_C: "status",
+            instance_D: "name",
+            instance_E: "email",
+            instance_F: "2023-04-01",
+            info_A: "lot",
+            info_B: "projectNumber",
+            info_C: "title",
+            info_D: "contractTitle",
+            info_E: "contractNumber",
+            info_F: "designRep",
+            info_G: "repAdjudicateur",
+            info_H: "issuer",
+            info_I: "sector",
+            info_J: "wbs",
+            info_K: "discipline",
+            info_L: "for",
+            info_M: "activity",
+            info_N: "isSubcontractor",
+            info_O: "sousActivity",
+            info_Q: "wbsText",
+            ref_chantier_pie: "reference",
+            categorie_nc: "category",
+            titre: "title",
+            date: "2023-04-01",
+            autre_ref: "otherReference",
+            date_reponse: "2023-04-01",
+            type_nc: "type",
+            sous_type_nc: "subType",
+            description: "description",
+            gerant_projet: "projectManagerEmail",
+            impact: "impact",
+            tables: [
+                { anchor: "plan_A", data: [{ plan_A: "description", plan_B: "actionPICEmail", plan_C: "2023-04-01", plan_D: "response", plan_E: "2023-04-01", plan_F: "mediaReadLink" }] },
+                { anchor: "resume_A", data: [{ resume_A: "description", resume_B: "2023-04-01", resume_C: "approvalStatusByQuality", resume_D: "approvalStatusByClient", resume_E: "Satisfaisant", resume_F: "approvalStatusByClient" }] },
+                { anchor: "doc_A", data: [{ doc_A: "fileName", doc_B: "fileDescription", doc_C: "comment", doc_D: "uploadedBy", doc_E: "2023-04-01", doc_F: "fileName", doc_G: "mediaReadLink", doc_H: "step" }] },
+                { anchor: "historique_A", data: [{ historique_A: "picEmail", historique_B: "2023-04-01", historique_C: "step", historique_D: "comment" }] },
+            ],
+            outputName: "MBH_NCR_RNC_Main_Form.pdf",
+            base64File: "base64string",
+        }),
     });
 });
+
+
--- src/index.ts
+++ src/index.ts
@@ -1,54 +1,84 @@
 import "@k2oss/k2-broker-core";
 
 metadata = {
-  systemName: "com.k2.example",
-  displayName: "Example Broker",
-  description: "An example broker that accesses JSONPlaceholder.",
+  systemName: "GenerateDocument",
+  displayName: "Generate Document",
+  description: "Service to generate document",
+  configuration: {
+    mbh: {
+      displayName: "MBH Workflow",
+      type: "string",
+    },
+    anotherWorkflow: {
+      displayName: "Another Workflow",
+      type: "string",
+    },
+    ServiceKey: {
+      displayName: "Service Key for document generation",
+      type: "number",
+      required: true,
+      value: "1",
+    },
+    stringFormat: {
+            displayName: "String Format",
+            type: "string",
+            value: "Alphanumeric"
+    }
+  },
 };
 
 ondescribe = async function ({ configuration }): Promise<void> {
+  const documentType = configuration["documentType"] ? configuration["documentType"].toString() : "pdf";
+
   postSchema({
     objects: {
-      todo: {
-        displayName: "TODO",
-        description: "Manages a TODO list",
+      document: {
+        displayName: "Document",
+        description: "Manages document generation",
         properties: {
           id: {
             displayName: "ID",
             type: "number",
           },
-          userId: {
-            displayName: "User ID",
-            type: "number",
-          },
-          title: {
-            displayName: "Title",
+          name: {
+            displayName: "Name",
             type: "string",
           },
-          completed: {
-            displayName: "Completed",
-            type: "boolean",
+          type: {
+            displayName: "Type",
+            type: "string",
+          },
+          base64Data: {
+            displayName: "Base64 Data",
+            type: "string",
+          },
+          jsonData1: {
+            displayName: "JSON Data 1",
+            type: "string",
+          },
+          jsonData2: {
+            displayName: "JSON Data 2",
+            type: "string",
+          },
+          jsonData3: {
+            displayName: "JSON Data 3",
+            type: "string",
+          },
+          jsonData4: {
+            displayName: "JSON Data 4",
+            type: "string",
+          },
+          documentType: {
+            displayName: "Document Type",
+            type: "string",
           },
         },
         methods: {
-          get: {
-            displayName: "Get TODO",
-            type: "read",
-            inputs: ["id"],
-            outputs: ["id", "userId", "title", "completed"],
-          },
-          getParams: {
-            displayName: "Get TODO",
-            type: "read",
-            parameters: {
-              pid: {
-                displayName: "param1",
-                description: "Description Of Param 1",
-                type: "number",
-              },
-            },
-            requiredParameters: ["pid"],
-            outputs: ["id"],
+          generate: {
+            displayName: "Generate Document",
+            type: "execute",
+            inputs: ["id", "name", "type", "base64Data", "jsonData1", "jsonData2", "jsonData3", "jsonData4", "documentType"],
+            outputs: ["jsonResult"],
           },
         },
       },
@@ -65,72 +95,160 @@ onexecute = async function ({
   schema,
 }): Promise<void> {
   switch (objectName) {
-    case "todo":
-      await onexecuteTodo(methodName, properties, parameters);
+    case "document":
+      await onexecuteDocument(methodName, properties, parameters, configuration);
       break;
     default:
       throw new Error("The object " + objectName + " is not supported.");
   }
 };
 
-async function onexecuteTodo(
+async function onexecuteDocument(
   methodName: string,
   properties: SingleRecord,
-  parameters: SingleRecord
+  parameters: SingleRecord,
+  configuration: SingleRecord
 ): Promise<void> {
   switch (methodName) {
-    case "get":
-      await onexecuteTodoGet(properties);
-      break;
-    case "getParams":
-      await onexecuteTodoGetWithParams(parameters);
+    case "generate":
+      await onexecuteGenerateDocument(properties, configuration);
       break;
     default:
       throw new Error("The method " + methodName + " is not supported.");
   }
 }
 
-function onexecuteTodoGet(properties: SingleRecord): Promise<void> {
-  return new Promise<void>((resolve, reject) => {
-    var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function () {
-      try {
-        if (xhr.readyState !== 4) return;
-        if (xhr.status !== 200)
-          throw new Error("Failed with status " + xhr.status);
+async function onexecuteGenerateDocument(
+  properties: SingleRecord,
+  configuration: SingleRecord
+): Promise<void> {
+  try {
+    const workflowType = properties["type"].toString(); 
+    const documentType = properties["documentType"].toString(); 
+    const jsonResult = await buildJsonRequest(properties, workflowType, documentType);
 
-        var obj = JSON.parse(xhr.responseText);
-        postResult({
-          id: obj.id,
-          userId: obj.userId,
-          title: obj.title,
-          completed: obj.completed,
-        });
-        resolve();
-      } catch (e) {
-        reject(e);
-      }
-    };
-
-    if (typeof properties["id"] !== "number")
-      throw new Error('properties["id"] is not of type number');
-    xhr.open(
-      "GET",
-      "https://jsonplaceholder.typicode.com/todos/" +
-        encodeURIComponent(properties["id"])
-    );
-    xhr.setRequestHeader("test", "test value");
-    xhr.send();
-  });
+    postResult({
+      jsonResult: JSON.stringify(jsonResult),
+    });
+  } catch (error) {
+    throw new Error("Error generating document: " + error.message);
+  }
 }
 
-function onexecuteTodoGetWithParams(parameters: SingleRecord): Promise<void> {
-  return new Promise<void>((resolve, reject) => {
+function buildJsonRequest(properties: SingleRecord, workflowType: string, documentType: string): Promise<any> {
+  switch (workflowType) {
+    case "mbh":
+      return buildMbhJson(properties, documentType);
+    case "anotherWorkflow":
+      return Promise.resolve({ message: "Another workflow not implemented yet." });
+    default:
+      return Promise.reject(new Error("Unsupported workflow type: " + workflowType));
+  }
+}
+
+function buildMbhJson(properties: SingleRecord, documentType: string): Promise<any> {
+  return new Promise<any>((resolve, reject) => {
     try {
-      postResult({
-        id: parameters["pid"],
-      });
-      resolve();
+      const payload1 = JSON.parse(properties["jsonData1"].toString());
+      const payload2 = JSON.parse(properties["jsonData2"].toString());
+      const payload3 = JSON.parse(properties["jsonData3"].toString());
+      const payload4 = JSON.parse(properties["jsonData4"].toString());
+
+      const action_plan = payload2.value;
+      const history = payload3.value;
+      const document_attach = payload4.value;
+
+      console.log("document_attach", document_attach);
+      const documentMap = document_attach.reduce((map, document) => {
+        map[document.NCR_ID] = document["File@odata.mediaReadLink"];
+        return map;
+      }, {});
+
+      const tableDataPlan = action_plan.map((action) => ({
+        plan_A: action.MBH_NCR_Description || "",
+        plan_B: action.MBH_NCR_ActionPICEmail || "",
+        plan_C: action.MBH_NCR_PlanDate.substring(0, 10) || "",
+        plan_D: action.MBH_NCR_Response || "",
+        plan_E: action.MBH_NCR_ExectionDate.substring(0, 10) || "",
+        plan_F: documentMap[action.MBH_NCR_NCRID_fk] || "",
+      }));
+
+      const tableDataAction = action_plan.map((resume) => ({
+        resume_A: resume.MBH_NCR_Description || "",
+        resume_B: resume.MBH_NCR_PlanDate.substring(0, 10) || "",
+        resume_C: resume.MBH_NCR_ActionPlanApprovalStatusByQuality || "",
+        resume_D: resume.MBH_NCR_ActionPlanApprovalStatusByClient || "",
+        resume_E: resume.MBH_NCR_ActionResponseApprovalStatusByPlanPIC === "Oui" ? "Satisfaisant" : "",
+        resume_F: resume.MBH_NCR_ActionResponseApprovalStatusByClient || "",
+      }));
+
+      const tableDataHistory = history.map((history) => ({
+        historique_A: history.ODataProperty__GCPC_NCR_PICEmail || "",
+        historique_B: history.GCPC_NCR_ResponseDate.substring(0, 10) || "",
+        historique_C: history.GCPC_NCR_Step || "",
+        historique_D: history.GCPC_NCR_Comment || "",
+      }));
+
+      const tableDataAttachdocument = document_attach.map((document) => ({
+        doc_A: document.FileName || "",
+        doc_B: document.FileDescription || "",
+        doc_C: document.Comment || "",
+        doc_D: document.UploadedBy || "",
+        doc_E: document.Upload_Date.substring(0, 10) || "",
+        doc_F: document.FileName || "",
+        doc_G: document["File@odata.mediaReadLink"] || "",
+        doc_H: document.Step || "",
+      }));
+
+      console.log("payload1", payload1);
+
+      const jsonObject = {
+        instance_A: payload1.UCI || "",
+        instance_B: "MBH NCR",
+        instance_C: payload1.NCRStatus || "",
+        instance_D: payload1.InitiatorName || "",
+        instance_E: payload1.InitiatorEmail || "",
+        instance_F: payload1.NCRRequestDate.substring(0, 10) || "",
+        info_A: payload1.NCRLot || "",
+        info_B: payload1.NCRProjectNumber || "",
+        info_C: payload1.ProjectTitle || "",
+        info_D: payload1.NCRContractTitle || "",
+        info_E: payload1.NCRContractNumber || "",
+        info_F: payload1.STMDesignRepresentative || "",
+        info_G: payload1.RepresentativeAdjudicateur || "",
+        info_H: payload1.NCRIssuer || "",
+        info_I: payload1.NCRSector || "",
+        info_J: payload1.NCRWBS || "",
+        info_K: payload1.NCRDiscipline || "",
+        info_L: payload1.MBH_NCR_For || "",
+        info_M: payload1.MBH_NCR_Activity || "",
+        info_N: payload1.MBH_NCR_IsSubcontractor || "",
+        info_O: payload1.MBH_NCR_SousActivity || "",
+        info_Q: payload1.WBSTEXT || "",
+        ref_chantier_pie: payload1.MBH_NCR_Reference || "",
+        categorie_nc: payload1.NCRCategory || "",
+        titre: payload1.NCRTitle || "",
+        date: payload1.NCRDate.substring(0, 10) || "",
+        autre_ref: payload1.NCROtherReference || "",
+        date_reponse: payload1.NCRResponseRequiredBy.substring(0, 10) || "",
+        type_nc: payload1.MBH_NCR_Type || "",
+        sous_type_nc: payload1.MBH_NCR_SubType || "",
+        description: payload1.NCRDescription || "",
+        gerant_projet: payload1.MBH_NCR_ProjectManagerEmail || "",
+        impact: payload1.MBH_NCR_ImpactPlanAsBuilt || "",
+        tables: [
+          { anchor: "plan_A", data: tableDataPlan },
+          { anchor: "resume_A", data: tableDataAction },
+          { anchor: "doc_A", data: tableDataAttachdocument },
+          { anchor: "historique_A", data: tableDataHistory },
+        ],
+        outputName: documentType === "pdf" ? "MBH_NCR_RNC_Main_Form.pdf" : "MBH_NCR_RNC_Main_Form.docx",
+        base64File: properties["base64Data"].toString(),
+      };
+
+      console.log("jsonobject", jsonObject);
+
+      resolve(jsonObject);
     } catch (e) {
       reject(e);
     }
